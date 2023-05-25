752230254e66f9fdc247eaf6293ad23470a5ae1f
andrey.logvinov <andrey.logvinov@oskelly.ru>
2023-05-26 00:26:46 +0300

    redesign

--- src/pages/EthersPage.vue
+++ src/pages/EthersPage.vue
@@ -6,8 +6,8 @@
 import { ethers } from 'ethers';
 
 // const provider = new ethers.JsonRpcProvider('ethers/address/0x5724539BDE57e87f729AF6a7b6c15303365Ffd65#code#F1#L1');
-const provider = new ethers.EtherscanProvider('');
-const signer = await provider.getSigner();
+// const provider = new ethers.EtherscanProvider('');
+// const signer = await provider.getSigner();
 
-console.log(signer);
+// console.log(signer);
 </script>
