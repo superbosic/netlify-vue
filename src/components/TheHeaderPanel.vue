0afbc6f6015e2ebce67435841da8443ffdc97152
andrey.logvinov <andrey.logvinov@oskelly.ru>
2023-06-09 00:13:25 +0300

    web3oauth add connectedAdapterName

--- src/components/TheHeaderPanel.vue
+++ src/components/TheHeaderPanel.vue
@@ -30,14 +30,28 @@
 
 <script lang="ts" setup>
 import { useRouter } from 'vue-router';
+import { Web3Auth } from '@web3auth/modal';
 import { useAccountStore } from '@/store/account';
 import { RouteNames } from '@/router/routeNames';
 
 const { user, logout } = useAccountStore();
 const router = useRouter();
 
-function logoutClick() {
+async function logoutClick() {
+  const web3auth = new Web3Auth({
+    clientId: import.meta.env.VITE_WEB3OAUTH_CLIENT_ID, // Get your Client ID from Web3Auth Dashboard
+    chainConfig: {
+      chainNamespace: 'eip155',
+      chainId: '0x1',
+    },
+  });
+
+  await web3auth.initModal();
+  await web3auth.connect();
+  await web3auth.logout();
+
   logout();
+
   router.replace({
     name: RouteNames.AccountLogin,
     query: { redirect: router.currentRoute.value.fullPath },
